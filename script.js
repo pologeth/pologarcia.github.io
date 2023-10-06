e0d3cc0cd1f5f62f4e98a4fbb70f0bc431c2fcc1
pologeth <134503786+pologeth@users.noreply.github.com>
2023-10-06 18:47:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const participantes = [
     { nombre: "Albert Rovira", puntos: 0 },
     { nombre: "Alex Burgos", puntos: 0 },
     { nombre: "Alex Marin", puntos: 0 },
-    { nombre: "Andre Garcia", puntos: 0 },
+    { nombre: "Andrea Garcia", puntos: 0 },
     { nombre: "Anna de Sousa", puntos: 0 }, // Cambia el puntaje de Anna
     { nombre: "Claudia Alcober", puntos: 0 },
     { nombre: "Clara Torner", puntos: 0 },
@@ -13,7 +13,7 @@ const participantes = [
     { nombre: "Eric Ribert", puntos: 0 },
     { nombre: "Francis Quinn", puntos: 0 },
     { nombre: "Gael Benet", puntos: 0 },
-    { nombre: "Gerar Garces", puntos: 0 },
+    { nombre: "Gerard Garces", puntos: 0 },
     { nombre: "Hanna Matas", puntos: 0 },
     { nombre: "Jack Daniels", puntos: 0 },
     { nombre: "Jan Benito", puntos: 0 },
